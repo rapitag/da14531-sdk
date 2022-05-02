9a6d232eee8eb06e643a811e9f5f616fb99f1e02
Harry Hornbacher <hhornbacher@rapitag.com>
2022-05-02 10:27:53 +0200

    Add safe wrappers for co_list_init, co_list_is_empty and co_list_pop_front

--- src/platform/core_modules/common.rs
+++ src/platform/core_modules/common.rs
@@ -1,1 +1,21 @@
-pub use crate::bindings::{ADV_DATA_LEN, SCAN_RSP_DATA_LEN};
+pub use crate::bindings::{
+    co_list as CoList, co_list_hdr as CoListHdr, ADV_DATA_LEN, SCAN_RSP_DATA_LEN,
+};
+
+pub fn co_list_init(list: &mut CoList) {
+    unsafe {
+        crate::bindings::co_list_init(list as *mut _);
+    }
+}
+
+pub fn co_list_is_empty(list: &mut CoList) -> bool {
+    list.first == core::ptr::null_mut()
+}
+
+pub fn co_list_pop_front(list: &mut CoList) -> &mut CoListHdr {
+    unsafe {
+        crate::bindings::co_list_pop_front(list as *mut _)
+            .as_mut()
+            .unwrap()
+    }
+}

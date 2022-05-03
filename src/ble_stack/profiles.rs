9557c74c22ab8d2907e2776b15ecc7e75048b203
Harry Hornbacher <hhornbacher@rapitag.com>
2022-05-03 17:11:12 +0200

    Export types and wrap prf_get_task_from_id

--- src/ble_stack/profiles.rs
+++ src/ble_stack/profiles.rs
@@ -1,7 +1,2 @@
-use crate::platform::core_modules::ke::msg::{KeMsgId, KeTaskId};
-
 pub mod custom;
-
-pub fn prf_get_task_from_id(msg_id: KeMsgId) -> KeTaskId {
-    unsafe { crate::bindings::prf_get_task_from_id(msg_id) }
-}
+pub mod prf;

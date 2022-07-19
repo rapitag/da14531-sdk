0b2c7604fe24c7e642a17cd7ac9295f56f510b64
Harry Hornbacher <hhornbacher@rapitag.com>
2022-07-19 08:29:51 +0200

    Add GPIO_EnableIRQ and GPIO_RegisterCallback

--- src/platform/driver.rs
+++ src/platform/driver.rs
@@ -10,3 +10,24 @@ pub mod syscntl {
         }
     }
 }
+
+pub mod gpio {
+    #[inline]
+    pub fn GPIO_EnableIRQ(pin: u8, irq: u8, low_input: bool, release_wait: bool, debounce: u8) {
+        unsafe {
+            crate::bindings::GPIO_EnableIRQ(
+                0,
+                pin as u32,
+                irq as i32,
+                low_input,
+                release_wait,
+                debounce,
+            );
+        }
+    }
+
+    #[inline]
+    pub fn GPIO_RegisterCallback(irq: u8, callback: unsafe extern "C" fn()) {
+        unsafe { crate::bindings::GPIO_RegisterCallback(irq as i32, Some(callback)) }
+    }
+}

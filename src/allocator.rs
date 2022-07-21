81d3a38392ec951b430dd73e5e09a79c55285511
Harry Hornbacher <hhornbacher@rapitag.com>
2022-07-21 11:52:23 +0200

    Integrate allocator implementation

---
+++ src/allocator.rs
@@ -0,0 +1,17 @@
+use core::alloc::{GlobalAlloc, Layout};
+
+use crate::bindings::{ke_free, ke_malloc};
+
+pub struct Da14531Allocator;
+
+impl Da14531Allocator {}
+
+unsafe impl GlobalAlloc for Da14531Allocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        unsafe { ke_malloc(layout.size() as u32, 3) as *mut u8 }
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        unsafe { ke_free(ptr as *mut cty::c_void) }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+extern crate alloc;
+
 pub mod bindings;
 
 pub mod app;
@@ -10,6 +12,7 @@ pub mod app_modules;
 pub mod ble_stack;
 pub mod platform;
 pub mod stdlib;
+pub mod allocator;
 
 pub use paste::paste;
 

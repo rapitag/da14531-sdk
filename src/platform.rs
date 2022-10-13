f762d8a27098c056227291d9219673b3e72a9c16
Harry Hornbacher <hhornbacher@rapitag.com>
2022-10-13 09:36:45 +0200

    Wrap otp_cs_get_adc_trim_val and otp_cs_get_adc_25_cal

--- src/platform.rs
+++ src/platform.rs
@@ -2,3 +2,17 @@ pub mod arch;
 pub mod core_modules;
 pub mod driver;
 pub mod system_library;
+pub mod utilities {
+    pub mod otp_cs {
+
+        #[inline]
+        pub fn otp_cs_get_adc_trim_val() -> u16 {
+            unsafe { crate::bindings::otp_cs_get_adc_trim_val() }
+        }
+
+        #[inline]
+        pub fn otp_cs_get_adc_25_cal() -> u16 {
+            unsafe { crate::bindings::otp_cs_get_adc_25_cal() }
+        }
+    }
+}

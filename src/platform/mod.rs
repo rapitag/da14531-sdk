93b98859d93ad49d09a0da4fcb1aba44a04177ad
Harry Hornbacher <hhornbacher@rapitag.com>
2024-06-24 17:50:14 +0200

    Add wrappers for otp_cs_get_* functions

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -9,5 +9,25 @@ pub mod utilities {
         pub fn otp_cs_get_adc_trim_val() -> u16 {
             unsafe { crate::bindings::otp_cs_get_adc_trim_val() }
         }
+
+        #[inline]
+        pub fn otp_cs_get_adc_single_ge() -> i16 {
+            unsafe { crate::bindings::otp_cs_get_adc_single_ge() }
+        }
+
+        #[inline]
+        pub fn otp_cs_get_adc_single_offset() -> i16 {
+            unsafe { crate::bindings::otp_cs_get_adc_single_offset() }
+        }
+
+        #[inline]
+        pub fn otp_cs_get_adc_offsh_ge() -> i16 {
+            unsafe { crate::bindings::otp_cs_get_adc_offsh_ge() }
+        }
+
+        #[inline]
+        pub fn otp_cs_get_adc_offsh_offset() -> i16 {
+            unsafe { crate::bindings::otp_cs_get_adc_offsh_offset() }
+        }
     }
 }
